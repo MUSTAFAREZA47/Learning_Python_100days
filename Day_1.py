bd22f8c30252ea63eb06cccbd1102ef78ebcb5bd
Ahmed Reza <mustafareza47@gmail.com>
2023-09-09 23:29:26 +0530

    first commit

---
+++ Day_1.py
@@ -0,0 +1,30 @@
+# Print Function
+print('Hello World!\nHello World!')
+
+print("Ahmed" + " " + "Reza")
+
+# Input Function
+input('What is your name?')
+print("Hello" + input("what is your name?"))
+
+# Numbers of character in your name generator 
+print(len(input('what is your name?')))
+
+# Interchanging the variable values 
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+c = b
+
+print("a = " + a)
+print("b = " + b)
+
+# Band Name Generator
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's name of the city you grew up in?")
+pet_name = input("what is your pet name?")
+
+print("Your Band Name is " + city_name + " " + pet_name)
