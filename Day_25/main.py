f2b8007b9eeccf0669bf138d19743370794e5382
mustafareza47 <MUSTAFAREZA47@GMAIL.COM>
2023-12-18 11:50:59 +0530

    exploring pandas lib in depth

--- Day_25/main.py
+++ Day_25/main.py
@@ -20,5 +20,24 @@
 # using pandas library to read data more appealing
 import pandas
 
-data = pandas.read_csv("../Day_25/weather.csv")
-print(data['MinTemp'])
+# data = pandas.read_csv("../Day_25/weather.csv")
+# print(data['MinTemp'])
+# data_json = data.to_json("../Day_25/weather.json")
+
+# average_temp = data["MinTemp"].mean()
+# print(round(average_temp, 2))
+# print(data[data["RainTomorrow"] == "Yes"])
+
+data = pandas.read_csv("../Day_25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_fur_color = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_fur_color = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_fur_color = len(data[data["Primary Fur Color"] == "Black"])
+
+fur_color_count = {
+    "Primary Fur Color": [" Gray", "Cinnamon", "Black"],
+    "Number of Squarel": [gray_fur_color, cinnamon_fur_color, black_fur_color]
+}
+
+pandas.DataFrame(fur_color_count).to_csv("../Day_25/fur_color_count.csv")
+
+print(fur_color_count)
