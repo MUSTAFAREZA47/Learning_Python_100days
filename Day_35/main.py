615af85870074f5bb30d152b5a4ff9fe237839d2
mustafareza47 <MUSTAFAREZA47@GMAIL.COM>
2024-01-05 00:00:19 +0530

    working with env file

--- Day_35/main.py
+++ Day_35/main.py
@@ -1,10 +1,15 @@
 import requests
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env file
+load_dotenv()
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "__YOUR_OWM_API_KEY__"
-account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
-auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
+api_key = os.getenv("OWM_API_KEY")
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
 
 weather_params = {
     "lat": 46.947975,
@@ -16,19 +21,20 @@ weather_params = {
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-# print(weather_data["list"][0]["weather"][0]["id"])
+print(weather_data["list"][0]["weather"][0]["id"])
 
 will_rain = False
 for hour_data in weather_data["list"]:
     condition_code = hour_data["weather"][0]["id"]
-    if int(condition_code) < 700:
+    if int(condition_code) < 805:
         will_rain = True
 if will_rain:
     client = Client(account_sid, auth_token)
     message = client.messages \
         .create(
         body="It's going to rain today. Remember to bring an ☔️",
-        from_="YOUR TWILIO VIRTUAL NUMBER",
-        to="YOUR TWILIO VERIFIED REAL NUMBER"
+        from_="+14083200029",
+        to="+918800753674"
     )
-    print(message.status)
+    print(message.sid)
+    # print("hello")
